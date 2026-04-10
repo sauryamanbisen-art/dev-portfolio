520107a635a98294de50fb3a92b781276ea34f29
Sauryaman Bisen <sauryamanbisen@gmail.com>
2026-04-11 00:07:53 +0530

    ok

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -2,7 +2,7 @@
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
   document.querySelector(".style-switcher").classList.toggle("open");
-});
+}); 
 // hide style - switcher on scroll
 window.addEventListener("scroll", () => {
   if(document.querySelector(".style-switcher").classList.contains("open"))
